335b087a7bb2a335d203b078fc4f3c7fe78dfb60
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-12 09:59:05 -0800

    Scraping from first page of stack overflow jobs.

---
+++ scraper.py
@@ -0,0 +1,22 @@
+"""A web scraper for job postings on stack overflow filtering for python jobs."""
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+if __name__ == '__main__':
+    page = requests.get("https://stackoverflow.com/jobs")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    jobs = soup.find_all(href='/jobs/developer-jobs-using-python')
+    job_summaries = []
+    for job in jobs:
+        job_summaries.append(job.parent.parent.parent)
+
+    with open('jobs.csv', 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        for job in job_summaries:
+            title = job.find(class_='job-link').text
+            company = job.find(class_='-name').text
+            location = job.find(class_='-location').text
+            date_posted = job.find(class_='-posted-date').text
+            link = 'https://stackoverflow.com{}'.format(job.find(class_='job-link')['href'])
+            writer.writerow([title, company, location, date_posted, link])
